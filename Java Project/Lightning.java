571dc65faccf83c4871423787fe879fcc59921b9
cwstudent <courtney.webber@students.mq.edu.au>
2026-08-22 19:32:21 +1000

    Remove strikesPerSecond from superclass constructor

--- Java Project/Lightning.java
+++ Java Project/Lightning.java
@@ -3,7 +3,7 @@ public class Lightning extends Weather{
 	public int strikesPerSecond;
 
 	public Lightning(int strength, int duration, int[][] location, int strikesPerSecond){
-		super(strength, duration, location, strikesPerSecond);
+		super(strength, duration, location);
 		this.strikesPerSecond = strikesPerSecond;
 	}
 
