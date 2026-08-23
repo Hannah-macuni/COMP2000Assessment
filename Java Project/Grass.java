c38b34bc4272acb4ec9734921b23a8cef9a3eae2
Hai Ha Nguyen <haiha.nguyen@students.mq.edu.au>
2026-08-23 21:51:26 +1000

    add content for grass class

--- Java Project/Grass.java
+++ Java Project/Grass.java
@@ -1,3 +1,16 @@
-public class Grass {
-    
+public class Grass extends Vegetation {
+    private int density;
+
+    public Grass(int age, int fuel, int burnRate, double moisture, int density) {
+        super(age, fuel, burnRate, moisture);
+        this.density = density;
+    }
+
+    public int getDensity() {
+        return density;
+    }
+
+    public void setDensity(int density) {
+        this.density = density;
+    }
 }
