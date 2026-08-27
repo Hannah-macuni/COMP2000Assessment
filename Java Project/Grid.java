e6b14dc2f036b4ebb48b4073ea74c4c2fe168ccb
Joel-AndrewsMQ <joel.andrews@students.mq.edu.au>
2026-08-27 20:30:55 +1000

    Create default constructor for Grid

--- Java Project/Grid.java
+++ Java Project/Grid.java
@@ -1,3 +1,16 @@
-public class Grid {
-    
+import java.util.ArrayList;
+public class Grid<T> {
+    ArrayList<ArrayList<T>> grid;
+    static int dx;
+    static int dy;
+
+    public Grid(){
+        dx = 10;
+        dy = 10;
+        grid = new ArrayList<>(dx);
+        for (int i = 0; i < dx; i++) {
+            ArrayList<T> reference = grid.get(i);
+            reference = new ArrayList<T>();
+        }
+    }
 }
