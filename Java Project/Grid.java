bcf597f48523d330ba612f9e92e6408ab79b7149
Joel-AndrewsMQ <joel.andrews@students.mq.edu.au>
2026-08-29 15:33:37 +1000

    Implements Grid.neighbors()

--- Java Project/Grid.java
+++ Java Project/Grid.java
@@ -1,12 +1,13 @@
 import java.util.ArrayList;
 import java.util.List;
+
 public class Grid<T> {
     ArrayList<ArrayList<T>> grid;
     int dx;
     int dy;
 
-    public Grid(){
-        //Returns a Grid of type T with initial dimensions 10x10
+    public Grid() {
+        // Returns a Grid of type T with initial dimensions 10x10
         dx = 10;
         dy = 10;
         grid = new ArrayList<>(dx);
@@ -16,8 +17,8 @@ public class Grid<T> {
         }
     }
 
-    public Grid(int dx, int dy){
-        //Returns a Grid of type T with dimensions dx x dy
+    public Grid(int dx, int dy) {
+        // Returns a Grid of type T with dimensions dx x dy
         this.dx = dx;
         this.dy = dy;
         grid = new ArrayList<>(dx);
@@ -27,9 +28,9 @@ public class Grid<T> {
         }
     }
 
-    public Grid(Grid<T> g){
-        //Returns a Grid of type T with dimensions copied from another Grid.
-        //NB: This method only copies the size of the Grid, not any elements in it. -JA
+    public Grid(Grid<T> g) {
+        // Returns a Grid of type T with dimensions copied from another Grid.
+        // NB: This method only copies the size of the Grid, not any elements in it. -JA
         this.dx = g.dx;
         this.dy = g.dy;
         grid = new ArrayList<>();
@@ -39,15 +40,15 @@ public class Grid<T> {
         }
     }
 
-    public T getCell(int row, int column){
+    public T getCell(int row, int column) {
         return grid.get(row).get(column);
     }
 
-    public void setCell(int row, int column, T element){
+    public void setCell(int row, int column, T element) {
         grid.get(row).set(column, element);
     }
 
-    public void fill(T element){
+    public void fill(T element) {
         for (ArrayList<T> arrayList : grid) {
             for (int i = 0; i < arrayList.size(); i++) {
                 arrayList.set(i, element);
@@ -55,19 +56,36 @@ public class Grid<T> {
         }
     }
 
-    public boolean isInBounds(int row, int column){
+    public boolean isInBounds(int row, int column) {
         if (grid.size() > row + 1) {
             return false;
-        } else if(grid.get(row).size() > column + 1){
+        } else if (grid.get(row).size() > column + 1) {
             return false;
         }
         return true;
     }
 
-    public List<T> neighbors(int row, int column){
-        if(!isInBounds(row, column)) return null;
+    public List<T> neighbors(int row, int column) {
+        if (!isInBounds(row, column))
+            return null;
+
         ArrayList<T> list = new ArrayList<>();
-        if(true);
+        if (!isInBounds(row - 1, column - 1))
+            list.add(getCell(row - 1, column - 1));
+        if (!isInBounds(row - 1, column))
+            list.add(getCell(row - 1, column));
+        if (!isInBounds(row - 1, column + 1))
+            list.add(getCell(row - 1, column + 1));
+        if (!isInBounds(row, column - 1))
+            list.add(getCell(row, column - 1));
+        if (!isInBounds(row, column + 1))
+            list.add(getCell(row, column + 1));
+        if (!isInBounds(row + 1, column - 1))
+            list.add(getCell(row + 1, column - 1));
+        if (!isInBounds(row + 1, column))
+            list.add(getCell(row + 1, column));
+        if (!isInBounds(row + 1, column + 1))
+            list.add(getCell(row + 1, column + 1));
         return list;
     }
 }
