a6ed23f96458fb7af429dae54ad457ba50f6b2e4
flo-pandiana <florencia.pandiana@students.mq.edu.au>
2026-09-07 06:32:00 +1000

    Implement WeatherManager class

--- Java Project/WeatherManager.java
+++ Java Project/WeatherManager.java
@@ -1,8 +1,80 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class WeatherManager {
-    public boolean isActive(Weather w){
-        if(w.getDuration()<0){
+    ArrayList<Weather> weathers = new ArrayList<>();
+    private ForestFireSimulation simulation;
+
+    public WeatherManager(ForestFireSimulation simulation) {
+        this.simulation = simulation;
+    }
+
+    public boolean isActive(Weather w) {
+        if (w.getDuration() <= 0) {
             return false;
         }
+
         return true;
+    }
+
+    public void createWeather(String weather) {
+        Weather w = null;
+
+        switch (weather) {
+            case "Rain":
+                w = new Rain(8, 20, null);
+                break;
+            case "Heatwave":
+                w = new Heatwave(2, 20, null);
+                break;
+            case "Lightning":
+                w = new Lightning(10, 1, null, 3);
+                break;
+            default:
+                break;
+        }
+
+        weathers.add(w);
+    }
+
+    public void createWeather(String weather, String selectedDirection) {
+        Weather w = null;
+        int direction = 0;
+
+        switch (selectedDirection) {
+            case "East":
+                direction = 1;
+                break;
+            case "South":
+                direction = 2;
+                break;
+            case "West":
+                direction = 3;
+                break;
+            default:
+                break;
+        }
+
+        w = new Wind(8, 20, null, direction);
+        weathers.add(w);
+    }
+
+    public void advanceDecrement(Weather w) {
+        w.decrementDuration();
+    }
+
+    public void update() {
+        Iterator<Weather> it = weathers.iterator();
+
+        while (it.hasNext()) {
+            Weather w = it.next();
+            advanceDecrement(w);
+
+            if (!isActive(w)) {
+                it.remove();
+            } else {
+                w.affectSimulation(simulation);
+            }
+        }
     }
 }
