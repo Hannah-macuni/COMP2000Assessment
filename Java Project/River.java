a0a222f4177fb2a7f8fe0081dbce69c19107ac2d
Joel-AndrewsMQ <joel.andrews@students.mq.edu.au>
2026-09-07 15:42:14 +1000

    renames Cell.java, makes River a daughter of Terrain

--- Java Project/River.java
+++ Java Project/River.java
@@ -1,3 +1,15 @@
-public class River {
+public class River extends Terrain {
+
+    @Override
+    public void update() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
+
+    @Override
+    public Terrain clone() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'clone'");
+    }
     
 }
